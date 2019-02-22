6cbec4c2f0bb595d069ac94a6a46b64b03854936
lgarciaos <lgarciaos@protonmail.com>
2019-02-21 21:01:33 -0600

    faltaba ; en l7 tempoctc

--- practica2/temporizador_CTC/temporizador_CTC.ino
+++ practica2/temporizador_CTC/temporizador_CTC.ino
@@ -4,7 +4,7 @@ void setup() {
   DDRB = DDRB | B10000000;
   cli();
   TCCR1B= 0; TCCR1A=0;
-  TCCR1B |= B00001101
+  TCCR1B |= B00001101;
   OCR1A= 0x0F42;
   TIFR1 |= (1 << OCF1A);
   TIMSK1 |= (1 << OCIE1A);
